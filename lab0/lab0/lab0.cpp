83f1327c6511b1ca251a9d526a3f45f0590a7738
emmanuel asamani <asamanek1051@student.laccd.edu>
2025-02-15 15:59:30 -0800

    first commit

---
+++ lab0/lab0/lab0.cpp
@@ -0,0 +1,34 @@
+// EMMANUEL ASAMANI
+// Lab #0
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, product;
+
+        cout << "\nMy name is emmanuel asamani ";
+
+    cout << "\nEnter the first number > ";
+    cin >> num1;
+
+    cout << "\nEnter the second number > ";
+    cin >> num2;
+
+    product = num1 * num2;
+    cout << "\nThe product of " << num1 << " and " << num2 << " is " << product << endl;
+
+    return 0;
+}
+
+/*
+  
+My name is emmanuel asamani
+Enter the first number > 2
+
+Enter the second number > 2
+
+The product of 2 and 2 is 4
+*/
+
